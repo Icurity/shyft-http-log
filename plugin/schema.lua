9ad6e6ec9b0cd3d2a675e146a96d8c61805577b9
Mooketsi Regoeng <mooketsir@bbd.co.za>
2022-11-07 10:42:11 +0200

    changing name

--- plugin/schema.lua
+++ plugin/schema.lua
@@ -2,7 +2,7 @@ local typedefs = require "kong.db.schema.typedefs"
 local url = require "socket.url"
 
 return {
-  name = "kong-to-loki",
+  name = "shyft-http-log",
   fields = {
     { protocols = typedefs.protocols },
     {
